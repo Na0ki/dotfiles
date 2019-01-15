6052bc6dbb62744d570a0be48682a0d31538c101
ahiru <wellingtonnmaeda0524@gmail.com>
2019-01-16 08:37:52 +0900

    prune is deprecated (use cleanup instead)

--- zsh/zsh.d/mac/aliases.zsh
+++ zsh/zsh.d/mac/aliases.zsh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-alias brupdate="brew update && brew upgrade && brew cleanup && brew prune"
+alias brupdate="brew update && brew upgrade && brew cleanup"
 alias lsusb="system_profiler SPUSBDataType"
 alias airport=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport
